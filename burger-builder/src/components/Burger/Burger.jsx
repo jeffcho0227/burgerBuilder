9a3e38f68d9ad6b139eca01b3745118556fd45de
Jeff Cho <jeff@Jeffs-MacBook-Pro.local>
2019-07-08 21:35:53 -0700

    [prog] able to dynamically render ingredients -set BurgerBuilder as a statefull comp and pass the number of ingredients by mapping out as another component

--- burger-builder/src/components/Burger/Burger.jsx
+++ burger-builder/src/components/Burger/Burger.jsx
@@ -4,11 +4,18 @@ import style from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients.jsx'
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+          return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredients key={igKey + i} type={igKey} />
+          })
+        })
   return (
     <div className={style.Burger}>
       <BurgerIngredients type='bread-top'/>
-      <BurgerIngredients type='cheese'/>
-      <BurgerIngredients type='meat'/>
+      {/* <BurgerIngredients type='cheese'/>
+      <BurgerIngredients type='meat'/> */}
+      {transformedIngredients}
       <BurgerIngredients type='bread-bottom'/>
     </div>
   );
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -6,12 +6,20 @@ import Burger from '../../components/Burger/Burger.jsx';
 class BurgerBuilder extends Component {
   constructor() {
     super();
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 1, 
+        cheese: 2,
+        meat: 2
+      }
+    }
   }
 
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>BUild Controls</div>
       </Aux>
     );
