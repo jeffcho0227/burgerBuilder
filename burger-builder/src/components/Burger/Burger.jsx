a34681d3efd2f57628455b1893db5d55665b0ee9
Jeff Cho <jeff@Jeffs-MacBook-Pro.local>
2019-07-10 15:02:50 -0700

    [prog] calculate the ingredient sum dynmacially -mapped through the keys of the state and returned the burgeringredientw based on the ingredient type.

--- burger-builder/src/components/Burger/Burger.jsx
+++ burger-builder/src/components/Burger/Burger.jsx
@@ -4,18 +4,23 @@ import style from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients.jsx'
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
-        .map((igKey) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+        .map( igKey => {
           return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredients key={igKey + i} type={igKey} />
           })
         })
+        //concat the arrays using reduce
+        .reduce((pre, cur) => {
+          return pre.concat(cur)
+        }, []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please add ingredients</p>
+    }
   return (
     <div className={style.Burger}>
       <BurgerIngredients type='bread-top'/>
-      {/* <BurgerIngredients type='cheese'/>
-      <BurgerIngredients type='meat'/> */}
-      {transformedIngredients}
+        {transformedIngredients}
       <BurgerIngredients type='bread-bottom'/>
     </div>
   );
